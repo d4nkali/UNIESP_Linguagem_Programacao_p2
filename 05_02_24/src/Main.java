8cb102c3aec7f04978d232c90635eefcd109900a
d4nkali <d4n_kali@protonmail.com>
2024-02-05 20:49:40 -0300

    Primeiros codigos no Java

---
+++ 05_02_24/src/Main.java
@@ -0,0 +1,36 @@
+// Primeiros codigo no Java
+
+public  class Main {
+
+    public static void main(String[] args ) {
+
+        System.out.println("Olá mundo"); // Imprime Ola Mundo
+
+        // Codigo de variaveis int e calculo
+
+        int num1 = 90;
+        int num2 = 70;
+        int total;
+
+        total = num1 + num2;
+
+        System.out.println(total);
+
+        //
+        if (num1 > num2) {
+
+            System.out.println("é maior");
+
+        } else if (num1 == num2) {
+
+            System.out.println("é igual");
+
+        } else {
+
+            System.out.println("é menor");
+
+        }
+
+    }
+
+}
