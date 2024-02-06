586f1021e31ab902773ac88d7224a51f64ace5d8
d4nkali <d4n_kali@protonmail.com>
2024-02-05 21:13:27 -0300

    Adicionando Exercicios 1

---
+++ 05_02_24/src/Main_1.java
@@ -0,0 +1,19 @@
+// Exercicio 1
+
+public class Main_1 {
+
+    public static void main(String[] args) {
+
+    int salario_h = 15;
+    int trabalho_h = 40;
+    int calc;
+
+    calc = salario_h * trabalho_h;
+
+    System.out.println("O salario total é de: " + calc);
+
+    }
+
+}
+
+// FIM!
