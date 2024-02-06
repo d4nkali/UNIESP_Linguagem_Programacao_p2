719c053b67fe57a103782e6976d56b7698ee29b0
d4nkali <d4n_kali@protonmail.com>
2024-02-05 21:14:29 -0300

    Adicionando Exercicios 4

---
+++ 05_02_24/src/Main_4.java
@@ -0,0 +1,36 @@
+// Exercicio 4
+
+public class Main_4 {
+
+    public static void main(String[] args) {
+
+        int salario = 606;
+
+        if (salario <= 606) {
+
+            System.out.println("Isento do Leão");
+
+        }
+
+        else if ((salario > 901) && (salario <= 1500)) {
+
+            System.out.println("Leão foi bom e deu um desconto de 5%");
+
+        }
+
+        else if ((salario > 1501) && (salario <= 2500)) {
+
+            System.out.println("Leão foi bom e deu um desconto de 10%");
+
+        }
+
+        else {
+
+            System.out.println("Leão foi bom e deu um desconto de 20%");
+
+        }
+
+    }
+}
+
+// FIM!
