f6d5f5c6e128d04f2d2eeb609c442de51955fe80
d4nkali <d4n_kali@protonmail.com>
2024-02-05 21:14:04 -0300

    Adicionando Exercicios 3

---
+++ 05_02_24/src/Main_3.java
@@ -0,0 +1,30 @@
+// Exercicio 3
+
+public class Main_3 {
+
+    public static void main(String[] args) {
+
+        int nota_1 = 50;
+        int nota_2 = 70;
+        int nota_3 = 100;
+        int nota_f;
+
+        nota_f = (nota_1+nota_2+nota_3)/3;
+
+        if (nota_f >= 70){
+
+            System.out.println("Ele foi aprovado com media: "+nota_f);
+
+        }
+
+        else {
+
+            System.out.println("Ele foi para a final com media: "+nota_f);
+
+        }
+
+    }
+
+}
+
+// FIM!
