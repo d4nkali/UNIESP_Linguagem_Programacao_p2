8567515dfdfc8c2db759fa6cc78869a1694b9d71
d4nkali <d4n_kali@protonmail.com>
2024-02-05 21:13:49 -0300

    Adicionando Exercicios 2

---
+++ 05_02_24/src/Main_2.java
@@ -0,0 +1,26 @@
+// Exercicio 2
+
+public class Main_2 {
+
+    public static void main(String[] args) {
+
+        int mund_palmeiras = 0;
+        int mund_corinthians = 2;
+
+
+        if (mund_palmeiras > mund_corinthians) {
+
+            System.out.println("Palmeiras tem mais mundial");
+
+        }
+
+        else {
+
+                System.out.println("O Corinthians tem mais mundial e o Palmeiras não");
+
+        }
+
+    }
+}
+
+// FIM!
