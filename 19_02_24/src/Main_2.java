7f29ea27202267bc4c61844c14dcf51e7d3adec1
d4nkali <d4n_kali@protonmail.com>
2024-02-19 20:31:21 -0300

    Usando input do teclado

---
+++ 19_02_24/src/Main_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main_2 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        int x = sc.nextInt();
+        System.out.println(x);
+
+    }
+}
