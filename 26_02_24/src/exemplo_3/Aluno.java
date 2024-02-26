b0284e8d0fdd9fdd5ee25ad1550c619ddb8ed640
d4nkali <d4n_kali@protonmail.com>
2024-02-26 20:41:20 -0300

    Adicionando exemplo 3

---
+++ 26_02_24/src/exemplo_3/Aluno.java
@@ -0,0 +1,18 @@
+package exemplo_3;
+
+public class Aluno {
+    
+    String nome;
+
+    Double nota_1;
+    Double nota_2;
+    Double nota_3;
+    Double media;
+
+    public void calcular_media(){
+
+        media = (nota_1+nota_2+nota_3) / 3;
+
+    }
+
+}
