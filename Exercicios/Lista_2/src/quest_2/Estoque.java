02f37fc0154f9d7bba87ba194efc35ce2a2c1e5f
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-03-16 14:02:27 -0300

    Corrigindo questão 2

--- Exercicios/Lista_2/src/quest_2/Estoque.java
+++ Exercicios/Lista_2/src/quest_2/Estoque.java
@@ -4,7 +4,44 @@ public class Estoque {
     
     private int estoque_media; // Cria a variavel privada para calcular a media do estoque
 
-    public void calc_media(int estoque_max, int estoque_min, int quant_atual){ // Cria o método 'calc_media'
+    // Criam as variaveis do tipo inteiro
+    int quant_atual, estoque_min, estoque_max;
+
+    // Cria o get e set
+
+        public int getEstoque_media() {
+            return this.estoque_media;
+        }
+
+        public void setEstoque_media(int estoque_media) {
+            this.estoque_media = estoque_media;
+        }
+
+        public int getQuant_atual() {
+            return this.quant_atual;
+        }
+
+        public void setQuant_atual(int quant_atual) {
+            this.quant_atual = quant_atual;
+        }
+
+        public int getEstoque_min() {
+            return this.estoque_min;
+        }
+
+        public void setEstoque_min(int estoque_min) {
+            this.estoque_min = estoque_min;
+        }
+
+        public int getEstoque_max() {
+            return this.estoque_max;
+        }
+
+        public void setEstoque_max(int estoque_max) {
+            this.estoque_max = estoque_max;
+        }
+
+    public void calc_media(){ // Cria o método 'calc_media'
 
         estoque_media = (estoque_max + estoque_min) / 2; // Calcula a media do estoque e guarda a informação
 
