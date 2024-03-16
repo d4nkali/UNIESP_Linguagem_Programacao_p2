9f5f97639c25139ccf5a70f870578f9b9b763563
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-03-16 12:19:52 -0300

    Corrigindo questão 1

--- Exercicios/Lista_2/src/quest_1/Compra.java
+++ Exercicios/Lista_2/src/quest_1/Compra.java
@@ -2,14 +2,46 @@ package quest_1;
 
 public class Compra {
 
-    // Criam as variaveis privadas dos preços das maçãs
+    private double preco_normal = 1.30, preco_desconto = 1.00; // Criam as variaveis privadas dos preços das maçãs
 
-    private double preco_normal = 1.30;
-    private double preco_desconto = 1.00;
+    int quant_maca; // Cria a variavel para a quantidade de maças
+    double total; // Cria a variavel para o preço final
 
-    public double calc_maca(int quant_maca) { // Cria o método 'calc_maca'
+    // Cria os get e set das variaveis
 
-        double total; // Cria a variavel para o preço final
+        public double getTotal() {
+            return this.total;
+        }
+
+        public void setTotal(double total) {
+            this.total = total;
+        }
+
+        public double getPreco_normal() {
+            return this.preco_normal;
+        }
+
+        public void setPreco_normal(double preco_normal) {
+            this.preco_normal = preco_normal;
+        }
+
+        public double getPreco_desconto() {
+            return this.preco_desconto;
+        }
+
+        public void setPreco_desconto(double preco_desconto) {
+            this.preco_desconto = preco_desconto;
+        }
+
+        public int getQuant_maca() {
+            return this.quant_maca;
+        }
+
+        public void setQuant_maca(int quant_maca) {
+            this.quant_maca = quant_maca;
+        }    
+    
+    public void calc_maca() { // Cria o método 'calc_maca'
 
         if (quant_maca < 12) { // Se o usuario comprar menos de uma duzia, então:
 
@@ -23,7 +55,7 @@ public class Compra {
 
         }
 
-        return total;
+        setTotal(total); // Armazena o valor do total
 
     }
 
